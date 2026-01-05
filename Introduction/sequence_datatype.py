963a1626e501137c7988385ca99d933647936c61
user.name "palak khare <palakkhare1902@gmail.com>
2026-01-05 15:23:09 +0530

    data types set,frozenset ,dictionary

--- Introduction/sequence_datatype.py
+++ Introduction/sequence_datatype.py
@@ -12,3 +12,26 @@ print(my_list)
 
 my_tuple=('data1','data2','data3')
 print(my_tuple)
+
+
+"""
+Set data type -set(mutable) and frozen set(immutable)
+"""
+
+unique_number={1,25,2,2,4,4,1,3,8}  #set
+print(unique_number)
+
+
+immutable_number=frozenset([1,25,2,2,4,4,1,3,8]) #frozen set
+print(immutable_number)
+
+
+"""
+Mapping data types -dictionary(key value pairs),
+"""
+person={
+  'name':'gopal',
+  'age':100,
+  'number':213466
+}
+print(person)
