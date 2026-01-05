65dbb318817f8c2c21786f3a792756a1b380157c
user.name "palak khare <palakkhare1902@gmail.com>
2026-01-05 16:42:07 +0530

    while loop

---
+++ control statements/loops/while_loop.py
@@ -0,0 +1,19 @@
+i=1
+while i<=5:
+  print(i)
+  i+=1
+
+"""
+   1st iteration
+   i=1,i<=5
+
+  2nd iteration
+  i=2 ,2<=5
+
+  5th iteration
+  i=5 ,i<=5
+
+  6th iteration
+  i=6
+  i <=5 , 6<=5 false codition loop exit
+    """  
