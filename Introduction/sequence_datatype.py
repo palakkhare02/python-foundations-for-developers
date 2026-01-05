2bf4186c6c6578ee69ac118b35b0368977fe3573
user.name "palak khare <palakkhare1902@gmail.com>
2026-01-05 15:15:25 +0530

    list tuple string

--- Introduction/sequence_datatype.py
+++ Introduction/sequence_datatype.py
@@ -1,8 +1,14 @@
 """
 ordered collection of data type 
 string -'abc',"bc".strings immutable.
-list ,tuple
+list- group of variable store in single variable using [] ,tuple
 """
 
 text="this is a string"
 print(text)
+
+my_list=['data1','data2','data3']
+print(my_list)
+
+my_tuple=('data1','data2','data3')
+print(my_tuple)
