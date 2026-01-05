c4af0b17499b7d9d877504ec56b61093f9331e3a
user.name "palak khare <palakkhare1902@gmail.com>
2026-01-05 15:00:04 +0530

    variables ,comments

---
+++ Introduction/comments.py
@@ -0,0 +1,10 @@
+# single line comment
+"""
+This is multiline comment.
+comments are ignored by interpreter.
+"""
+
+'''
+This is also multi line comments
+'''
+
---
+++ Introduction/variable.py
@@ -0,0 +1,5 @@
+# variable declaration
+name='Palak' #string
+age =25       #int
+height=6.5    #float
+print(name,age,height)
