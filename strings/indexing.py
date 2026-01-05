7ab8efc87adc6292e41a1d31e0a601febdcad1f8
user.name "palak khare <palakkhare1902@gmail.com>
2026-01-05 19:00:24 +0530

    strings indexing,iterable

---
+++ strings/indexing.py
@@ -0,0 +1,20 @@
+""" 
+indexing starts with 0
++ve and -ve indexing
+a='python'
+p=0
+y=1
+t=2
+h=3
+o=4
+n=5
+"""
+a='python' #+ve indexing(left to right)
+print(a[0])
+print(a[1])
+print(a[5])
+
+b="Hello"     #-ve indexing starts with -1(right to left)
+print(a[-1])
+print(a[-2])
+print(a[-4])
---
+++ strings/strings.py
@@ -0,0 +1,10 @@
+# strings are indexed,immutable,iterable
+# create string 
+name1='palak'
+name2="pakhi"
+name3='''this is  multiline string'''
+name4="""this is also multiline string"""
+
+print(name1, name2, name3, name4)
+
+
