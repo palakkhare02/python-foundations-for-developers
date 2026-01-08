c7cf0efca02f8a968f864eed34b41659b80c38a9
user.name "palak khare <palakkhare1902@gmail.com>
2026-01-08 16:14:58 +0530

    check file exists or not

---
+++ file handling/check_file.py
@@ -0,0 +1,7 @@
+# check file exists or not
+import os
+if os.path.exists(r"C:\Users\apk14\OneDrive\Desktop\python\python-foundations-for-developers\file handling\write.txt"):
+  print("exists")
+
+else:
+    print("not available")
