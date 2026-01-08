15bd02c39e55764b01cc8b645e8e58a10402c797
user.name "palak khare <palakkhare1902@gmail.com>
2026-01-08 10:18:32 +0530

    global and local variable

---
+++ function/global_local.py
@@ -0,0 +1,9 @@
+# local variable-create and access in function
+def msg():
+    # local variable
+    choice="I love coding"
+    print(choice)
+msg()    
+# print(choice) error 
+choice1="hey how r you"  #global variable
+print(choice1)
