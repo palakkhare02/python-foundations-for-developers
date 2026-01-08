5b41ffbeb254a0e2d4347de97a1a82cafc39ab09
user.name "palak khare <palakkhare1902@gmail.com>
2026-01-08 10:12:39 +0530

    create function,parameter ,arguments

--- function/create.py
+++ function/create.py
@@ -1,6 +1,41 @@
-def greet():
+def greet(name): #name parameter
   """
   displaying a hi message to user #docstring 
   """
-  print("Hi")
-greet()  
+  print("Hi",name)
+greet("palak")  #palak -argument
+ 
+'''
+parameter-placeholder for values
+arguments- actual values that pass at the time of function calling
+3type of argument-positional,keyword ,default argument
+''' 
+
+# positional argument
+
+def greet1(name ,city): 
+  print(f'welcome {name} to the {city}')
+greet1("palak","mumbai")
+
+# keyword argument
+greet1(name="palak",city="mumbai")
+
+# default argument
+def greet1(name="palak",city="goa"): 
+  print(f'welcome {name} to the {city}')
+greet1("shyam","delhi")
+greet1()
+
+# return statement - use to exit a function and go back the place where return called
+
+def get_full_name(first_name,last_name):
+  '''return the full name , in a neated format'''
+  full_name=f'{first_name} {last_name}'
+  return full_name
+name=get_full_name("raju","awasthi")
+print(name)
+
+
+"""
+function naming,length of function, avoid global variable
+"""
