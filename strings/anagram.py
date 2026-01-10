ce2d6bc43df1e926572e26ce779720628d6fe5f1
Palak khare <palakkhare1902@gmail.com>
2026-01-10 12:31:22 +0000

    valid anagram question

---
+++ strings/anagram.py
@@ -0,0 +1,13 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.leetcode 242 question
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = {}
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+        for ch in t:
+            if ch not in count or count[ch] == 0:
+                return False
+            count[ch] -= 1
+        return True
