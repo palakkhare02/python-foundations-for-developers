38abbecfccab38891f413153828574e1ae1b525c
Palak khare <palakkhare1902@gmail.com>
2026-01-12 13:58:55 +0000

    instance variable and method

---
+++ oops/instance_variable_method.py
@@ -0,0 +1,14 @@
+### Instance Variable and Methods
+class Dog:
+    ## constructor
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+## create objects
+dog1=Dog("Buddy",3)
+print(dog1)
+print(dog1.name)
+print(dog1.age)
+    
+    
