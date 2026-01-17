fc7fd279e580248004093d14ed8e4f4ea7cab3c3
Palak khare <palakkhare1902@gmail.com>
2026-01-17 15:41:09 +0000

    magic methods

---
+++ oops/magic_method.py
@@ -0,0 +1,14 @@
+#### Magic Methods
+Magic methods in Python, also known as dunder methods (double underscore methods), are special methods that start and end with double underscores. These methods enable you to define the behavior of objects for built-in operations, such as arithmetic operations, comparisons, and more.
+
+##### Magic Methods
+Magic methods are predefined methods in Python that you can override to change the behavior of your objects. Some common magic methods include:
+
+'''
+__init__': Initializes a new instance of a class.
+__str__: Returns a string representation of an object.
+__repr__: Returns an official string representation of an object.
+__len__: Returns the length of an object.
+__getitem__: Gets an item from a container.
+__setitem__: Sets an item in a container.
+'''
